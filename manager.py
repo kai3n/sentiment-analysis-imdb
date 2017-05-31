fd212450ff6f21a27efd2c59fbde606ec47dbd5e
Mingun Pak <mingunpa@usc.edu>
2017-05-31 11:45:52 -0700

    modify: import module

--- manager.py
+++ manager.py
@@ -1,105 +1,11 @@
-# # -*- coding: utf-8 -*-
-#
-# import numpy as np
-# import gc
-# import re
-# import os
-# import random
-#
-# from keras.datasets import imdb
-# from keras.preprocessing import sequence
-# from keras.layers.embeddings import Embedding
-# from keras.models import Sequential
-from keras.models import model_from_json
-# from keras.layers import Dense
-# from keras.layers import Flatten
-#
-# class Manager(object):
-#     def __init__(self):
-#         pass
-#
-#     def load(self, positives, negatives):
-#         pos_dir = 'dataset/test/pos'
-#         neg_dir = 'dataset/test/neg'
-#         data = self.load_dir(pos_dir, 1, positives) + self.load_dir(neg_dir, 0, negatives)
-#         random.shuffle(data)
-#         return self.classify(data)
-#
-#     def load_dir(self, dirname, label, size):
-#         data = []
-#         files = os.listdir(dirname)
-#         random.shuffle(files)
-#         for fname in files[:size]:
-#             for line in open(os.path.join(dirname, fname)):
-#                 data.append((line, label))
-#         return data
-#
-#     def _load_imd(self, top_words=5000, max_words=500):
-#         """Returns x_train, y_train, x_test, y_text lists"""
-#
-#         seed = 7
-#         np.random.seed(seed)
-#         (self.x_train, self.y_train), (self.x_test, self.y_test) = imdb.load_data(num_words=top_words)
-#         self.x_train = sequence.pad_sequences(self.x_train, maxlen=max_words)
-#         self.x_test = sequence.pad_sequences(self.x_test, maxlen=max_words)
-#         return self.x_train, self.y_train, self.x_test, self.y_test
-#
-#     def train(self):
-#         """ trains model"""
-#
-#         # Fit the model
-#         self.model.fit(self.x_train, self.y_train, validation_data=(self.x_test, self.y_test),
-#                        epochs=2, batch_size=128, verbose=2)
-#         # Final evaluation of the model
-#         scores = self.model.evaluate(self.x_test, self.y_test, verbose=0)
-#         print("Accuracy: %.2f%%" % (scores[1]*100))
-#
-#     def store_model(self):
-#         """ serializes model"""
-#
-#         self.model_json = self.model.to_json()
-#         with open("model.json", "w") as json_file:
-#             json_file.write(self.model_json)
-#         # serialize weights to HDF5
-#         self.model.save_weights("model.h5")
-#         print("Saved model to disk")
-#
-#     def load_model(self):
-#         """load model """
-#
-#         self.json_file = open('model.json', 'r')
-#         self.loaded_model_json = self.json_file.read()
-#         self.json_file.close()
-#         self.model = model_from_json(self.loaded_model_json)
-#         # load weights into new model
-#         self.model.load_weights("model.h5")
-#         print("Loaded model from disk")
-#         self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-#
-#     def evaluate_model(self):
-#         """ evaluates model"""
-#
-#         self.load_model()
-#         score = self.model.evaluate(self.x_test, self.y_test, verbose=0)
-#         print("Accuracy: %.2f%%" % (score[1] * 100))
-#         gc.collect()
-#
-# if __name__ == "__main__":
-#     a = Manager()
+import os
 
-import time
+from keras.models import model_from_json
+from keras.preprocessing import sequence
+from keras.preprocessing.text import Tokenizer
+from single_model import SingleModel
 import numpy as np
 np.random.seed(1337)  # for reproducibility
-
-from keras.preprocessing import sequence
-from keras.models import Model, Sequential
-from keras.layers import Dense, Dropout, Embedding, LSTM, Input, merge, BatchNormalization
-from keras.datasets import imdb
-
-import os
-from keras.preprocessing.text import Tokenizer
-
-from single_model import SingleModel
 
 
 class Manager(object):
@@ -113,7 +19,7 @@ class Manager(object):
         # create int to word dictionary
         self.intToWord = {}
 
-    def load_dataset(self, max_features = 5000, max_len=500):
+    def load_dataset(self, max_features=5000, max_len=500):
         """Loads dataset. Before execute this function,
         get dataset and unzip: http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"""
 
@@ -199,19 +105,19 @@ class Manager(object):
                        epochs=2, batch_size=128, verbose=2)
         # Final evaluation of the model
         self.scores = self.model.evaluate(self.X_test, self.y_test, verbose=0)
-        print("Accuracy: %.2f%%" % (self.scores[1]*100))
+        print("Accuracy: %.2f%%" % (self.scores[1] * 100))
 
     def store_model(self, filename):
         """ serializes model"""
 
         model_json = self.model.to_json()
-        with open(filename + "_" + str(self.scores[1]*100) + "acc_" + "model.json", "w") as json_file:
+        with open(filename + "_" + str(self.scores[1] * 100) + "acc_" + "model.json", "w") as json_file:
             json_file.write(model_json)
         # serialize weights to HDF5
-        model.save_weights(filename + "_" + str(self.scores[1]*100) + "acc_" + "model.h5")
+        model.save_weights(filename + "_" + str(self.scores[1] * 100) + "acc_" + "model.h5")
         print("Saved model to disk")
 
-    def load_model(self,filename):
+    def load_model(self, filename):
         """load model. You should put filename without file extention"""
 
         json_file = open(filename + ".json", 'rt')
@@ -230,13 +136,3 @@ if __name__ == "__main__":
     manager.load_dataset()
     manager.train()
     manager.store_model("single")
-
-
-
-
-
-
-
-
-
-
